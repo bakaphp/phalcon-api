0103d3fc4ffe191080f7fd63c8aa65ab5a06e29a
Rafael White <rwhite@mctekk.com>
2020-08-04 16:38:31 -0400

    Change to unit Tester

--- tests/unit/BootstrapCest.php
+++ tests/unit/BootstrapCest.php
@@ -3,20 +3,18 @@
 namespace Gewaer\Tests\unit;
 
 use function Baka\appPath;
-use CliTester;
 use Codeception\Util\HttpCode;
+use UnitTester;
 
 class BootstrapCest
 {
-    public function checkBootstrap(CliTester $I)
+    public function checkBootstrap(UnitTester $I)
     {
         ob_start();
         require appPath('api/public/index.php');
         $actual = ob_get_contents();
         ob_end_clean();
         $results = json_decode($actual, true);
-        print_r($results);
-        die();
         $I->assertEquals('1.0', $results['jsonapi']['version']);
         $I->assertEmpty($results['data']);
         $I->assertEquals(HttpCode::getDescription(404), $results['errors']['message']);
