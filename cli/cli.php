54a8b421ac9158958ebf70c554211aed3fd63a3b
mctekk <max@mctekk.com>
2020-08-08 15:33:37 -0400

    Add disable for coroutines

--- cli/cli.php
+++ cli/cli.php
@@ -6,7 +6,9 @@ use Swoole\Runtime;
 require_once __DIR__ . '/../library/Core/autoload.php';
 
 //Allow the user to use Swoole Coroutines in our CLI Space
-Runtime::enableCoroutine();
+if ((bool) envValue('SWOOLE_COROUTINE', 1)) {
+    Runtime::enableCoroutine();
+}
 
 go(function () use ($argv) {
     $cli = new Cli();
